832f2dcd37ceca4cd102a28eb7de31ace4f1e6ce
andersoncbs <andersoncbs@gmail.com>
2018-12-04 16:16:24 -0200

    Curso android

---
+++ MensagensToast/app/src/main/java/com/andersonsouza/mensagenstoast/MainActivity.java
@@ -0,0 +1,27 @@
+package com.andersonsouza.mensagenstoast;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button botao;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        botao = findViewById(R.id.buttonClicarAqui);
+
+        botao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Mensagem Toast!", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
