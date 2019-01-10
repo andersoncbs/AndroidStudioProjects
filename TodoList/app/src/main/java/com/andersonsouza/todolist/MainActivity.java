faab25affeedff6720d687d43cbd0ad9a539eda8
04523987971 <anderson.souza@serpro.gov.br>
2019-01-10 13:05:39 -0200

    todoList

--- TodoList/app/src/main/java/com/andersonsouza/todolist/MainActivity.java
+++ TodoList/app/src/main/java/com/andersonsouza/todolist/MainActivity.java
@@ -7,9 +7,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends Activity {
 
@@ -17,6 +21,8 @@ public class MainActivity extends Activity {
     private Button botaoAdicionar;
     private ListView listView;
     private SQLiteDatabase bancoDados;
+    private ArrayAdapter<String> itensAdaptador;
+    private ArrayList<String> itens;
 
 
     @Override
@@ -26,11 +32,9 @@ public class MainActivity extends Activity {
 
         textoTarefa = findViewById(R.id.editTextTarefa);
         botaoAdicionar = findViewById(R.id.buttonAdicionar);
-        listView = findViewById(R.id.listViewId);
+        listView = findViewById(R.id.listViewTarefas);
 
         montarBD();
-
-        carregarDadosBD();
 
         botaoAdicionar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,38 +42,55 @@ public class MainActivity extends Activity {
                 String textoDigitado = textoTarefa.getText().toString();
 
                 try {
-                    bancoDados.execSQL("INSERT INTO TAREFA (DESC_TAREFA) VALUES ('" + textoDigitado + "')");
+                    salvarTarefa(textoDigitado);
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
             }
         });
+
+        carregarDadosBD();
     }
 
     private void carregarDadosBD() {
+        itens = new ArrayList<>();
+        itensAdaptador = new ArrayAdapter<String>(getApplicationContext(),
+                android.R.layout.simple_list_item_2,
+                android.R.id.text2,
+                itens);
+
+        listView.setAdapter(itensAdaptador);
+
         try {
             Cursor cursor = bancoDados.rawQuery("SELECT * FROM TAREFA", null);
-            int indexId = cursor.getColumnIndex("ID");
             int indexDesc = cursor.getColumnIndex("DESC_TAREFA");
 
-            cursor.moveToFirst();
-            Log.i("###", " TESTE 1");
-            while (cursor != null) {
-                Log.i("###", " TESTE 2");
-                 Log.i("###", " Tarefa: " + cursor.getString(indexDesc));
-                cursor.moveToNext();
+            while (cursor.moveToNext()) {
+                itens.add(cursor.getString(indexDesc));
             }
-            Log.i("###", " TESTE 3");
         } catch (Exception e) {
-            Log.i("###", " ERRO" + e.getMessage());
             e.printStackTrace();
         }
     }
 
     private void montarBD() {
         bancoDados = openOrCreateDatabase("apptarefas", MODE_PRIVATE, null);
-
         bancoDados.execSQL("CREATE TABLE IF NOT EXISTS TAREFA (ID INTEGER PRIMARY KEY AUTOINCREMENT, DESC_TAREFA VARCHAR)");
+    }
+
+    private void salvarTarefa(String textoDigitado) {
+        try {
+            if (textoDigitado.equals("")) {
+                Toast.makeText(MainActivity.this, "Digite uma tarefa", Toast.LENGTH_SHORT).show();
+            } else {
+                bancoDados.execSQL("INSERT INTO TAREFA (DESC_TAREFA) VALUES ('" + textoDigitado + "')");
+                carregarDadosBD();
+                textoTarefa.setText("");
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
