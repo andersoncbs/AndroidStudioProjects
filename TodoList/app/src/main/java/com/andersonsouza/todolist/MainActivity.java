8d8ac0511a3d159f487d9939ee4197f793a8f030
andersoncbs <andersoncbs@gmail.com>
2019-01-10 22:09:17 -0200

    Update MainActivity.java

--- TodoList/app/src/main/java/com/andersonsouza/todolist/MainActivity.java
+++ TodoList/app/src/main/java/com/andersonsouza/todolist/MainActivity.java
@@ -3,10 +3,10 @@ package com.andersonsouza.todolist;
 import android.app.Activity;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -23,6 +23,7 @@ public class MainActivity extends Activity {
     private SQLiteDatabase bancoDados;
     private ArrayAdapter<String> itensAdaptador;
     private ArrayList<String> itens;
+    private ArrayList<Integer> ids;
 
 
     @Override
@@ -49,11 +50,21 @@ public class MainActivity extends Activity {
             }
         });
 
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                removerTarefa(ids.get(position));
+                return true;
+            }
+        });
+
         carregarDadosBD();
     }
 
     private void carregarDadosBD() {
         itens = new ArrayList<>();
+        ids = new ArrayList<>();
+
         itensAdaptador = new ArrayAdapter<String>(getApplicationContext(),
                 android.R.layout.simple_list_item_2,
                 android.R.id.text2,
@@ -62,20 +73,23 @@ public class MainActivity extends Activity {
         listView.setAdapter(itensAdaptador);
 
         try {
-            Cursor cursor = bancoDados.rawQuery("SELECT * FROM TAREFA", null);
+            Cursor cursor = bancoDados.rawQuery("SELECT CODIGO, DESC_TAREFA FROM TAREFA ORDER BY CODIGO DESC", null);
             int indexDesc = cursor.getColumnIndex("DESC_TAREFA");
+            int indexId = cursor.getColumnIndex("CODIGO");
 
             while (cursor.moveToNext()) {
                 itens.add(cursor.getString(indexDesc));
+                ids.add(Integer.parseInt(cursor.getString(indexId)));
             }
         } catch (Exception e) {
+            Log.e("###", e.getMessage());
             e.printStackTrace();
         }
     }
 
     private void montarBD() {
         bancoDados = openOrCreateDatabase("apptarefas", MODE_PRIVATE, null);
-        bancoDados.execSQL("CREATE TABLE IF NOT EXISTS TAREFA (ID INTEGER PRIMARY KEY AUTOINCREMENT, DESC_TAREFA VARCHAR)");
+        bancoDados.execSQL("CREATE TABLE IF NOT EXISTS TAREFA (CODIGO INTEGER PRIMARY KEY AUTOINCREMENT, DESC_TAREFA VARCHAR)");
     }
 
     private void salvarTarefa(String textoDigitado) {
@@ -88,6 +102,16 @@ public class MainActivity extends Activity {
                 textoTarefa.setText("");
             }
 
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    private void removerTarefa(Integer id) {
+        try {
+            bancoDados.execSQL("DELETE FROM TAREFA WHERE CODIGO =" + id);
+            carregarDadosBD();
         } catch (Exception e) {
             e.printStackTrace();
         }
