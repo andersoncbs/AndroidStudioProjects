bcd7d498b24a90b076fac3e0dc96a2b55e8957c1
04523987971 <anderson.souza@serpro.gov.br>
2019-01-24 13:05:20 -0200

    zap início

---
+++ WhatsAppAndsu/app/src/main/java/com/andersonsouza/whatsappandsu/MainActivity.java
@@ -0,0 +1,20 @@
+package com.andersonsouza.whatsappandsu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+
+    private DatabaseReference referenciaBD = FirebaseDatabase.getInstance().getReference();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        referenciaBD.child("pontos").setValue(100);
+    }
+}
