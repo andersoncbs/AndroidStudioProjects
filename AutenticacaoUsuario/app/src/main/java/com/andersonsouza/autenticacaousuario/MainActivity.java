ebcdf262f34d991cd80020d7e2d22096f10c3a58
04523987971 <anderson.souza@serpro.gov.br>
2019-01-25 12:20:16 -0200

    vai

---
+++ AutenticacaoUsuario/app/src/main/java/com/andersonsouza/autenticacaousuario/MainActivity.java
@@ -0,0 +1,103 @@
+package com.andersonsouza.autenticacaousuario;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth auth;
+
+    private static final String EMAIL = "andersoncbs@gmail.com";
+    private static final String SENHA = "and123";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        //início
+        auth = FirebaseAuth.getInstance();
+
+        auth.signOut();
+        if (auth.getCurrentUser() != null) {
+            Log.i("@@@", "usuário já logado");
+        }
+
+        //criarUsuario();
+        //logar();
+    }
+
+    private void criarUsuario() {
+        auth.createUserWithEmailAndPassword(EMAIL, SENHA)
+                .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Log.i("@@@", "criação usuário ok");
+                        } else {
+                            Log.i("@@@", "usuário error");
+                        }
+                    }
+                });
+    }
+
+    private void logar() {
+        auth.signInWithEmailAndPassword(EMAIL, SENHA + "1").addOnCompleteListener(MainActivity.this,
+                new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Log.i("@@@", "login ok");
+                        } else {
+                            Log.i("@@@", "login error");
+                        }
+                    }
+                });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
