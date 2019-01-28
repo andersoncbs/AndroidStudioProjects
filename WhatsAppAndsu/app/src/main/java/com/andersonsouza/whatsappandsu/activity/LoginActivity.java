c4ccad8e7cee1be042a8782df38c08743322c111
andersoncbs <andersoncbs@gmail.com>
2019-01-28 19:47:56 -0200

    zap

--- WhatsAppAndsu/app/src/main/java/com/andersonsouza/whatsappandsu/activity/LoginActivity.java
+++ WhatsAppAndsu/app/src/main/java/com/andersonsouza/whatsappandsu/activity/LoginActivity.java
@@ -2,14 +2,64 @@ package com.andersonsouza.whatsappandsu.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.andersonsouza.whatsappandsu.R;
+import com.github.rtoshiro.util.format.SimpleMaskFormatter;
+import com.github.rtoshiro.util.format.text.MaskTextWatcher;
+
+import java.util.Random;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText nome;
+    private EditText telefone;
+    private EditText ddd;
+    private EditText ddi;
+
+    private Button botaoCadastrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        nome = findViewById(R.id.editTextNome);
+        ddd = findViewById(R.id.editTextDDD);
+        ddi = findViewById(R.id.editTextDDI);
+        telefone = findViewById(R.id.editTelefone);
+        botaoCadastrar = findViewById(R.id.buttonCadastrar);
+
+        SimpleMaskFormatter smfDDI = new SimpleMaskFormatter("+NN");
+        SimpleMaskFormatter smfDDD = new SimpleMaskFormatter("NN");
+        SimpleMaskFormatter smfTelefone = new SimpleMaskFormatter("NNNNN-NNNN");
+
+        MaskTextWatcher mtwTelefone = new MaskTextWatcher(this.telefone, smfTelefone);
+        MaskTextWatcher mtwDDI = new MaskTextWatcher(this.ddi, smfDDI);
+        MaskTextWatcher mtwDDD = new MaskTextWatcher(this.ddd, smfDDD);
+
+        ddi.addTextChangedListener(mtwDDI);
+        ddd.addTextChangedListener(mtwDDD);
+        telefone.addTextChangedListener(mtwTelefone);
+
+        botaoCadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String nomeUsuario = nome.getText().toString();
+                String numTelefone =
+                        ddi.getText().toString() +
+                        ddd.getText().toString() +
+                        telefone.getText().toString();
+                String telefoneSemFormatacao = numTelefone
+                        .replaceAll("-", "").replaceAll("/+", "");
+
+                Random random = new Random(System.currentTimeMillis());
+                Integer numSorteado = random.nextInt(8999) + 1000;
+                String token = numSorteado.toString();
+
+            }
+        });
     }
 }
