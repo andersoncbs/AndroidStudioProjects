60e7a1b4f7f12a9ff9d2f75d963c4429c6547f22
04523987971 <anderson.souza@serpro.gov.br>
2019-02-05 13:46:27 -0200

    zap

--- WhatsAppAndsu/app/src/main/java/com/andersonsouza/whatsappandsu/activity/LoginActivity.java
+++ WhatsAppAndsu/app/src/main/java/com/andersonsouza/whatsappandsu/activity/LoginActivity.java
@@ -1,25 +1,82 @@
 package com.andersonsouza.whatsappandsu.activity;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.andersonsouza.whatsappandsu.R;
 import com.andersonsouza.whatsappandsu.config.ConfiguracaoFirebase;
+import com.andersonsouza.whatsappandsu.model.Usuario;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 
 public class LoginActivity extends AppCompatActivity {
 
-//    private DatabaseReference referenciaBD;
+    private EditText editEmail;
+    private EditText editSenha;
+    private Button botaoLogar;
+    private Usuario usuario;
+    private FirebaseAuth autenticacao;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-//        referenciaBD = ConfiguracaoFirebase.getFirebase();
-//        referenciaBD.child("pontos").setValue(200);
+        editEmail = findViewById(R.id.editEmail);
+        editSenha = findViewById(R.id.editSenha);
+        botaoLogar = findViewById(R.id.buttonLogar);
+
+        verificarUsuarioLogado();
+
+        botaoLogar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                usuario = new Usuario();
+                usuario.setEmail(editEmail.getText().toString());
+                usuario.setSenha(editSenha.getText().toString());
+
+                validarLogin();
+            }
+        });
+    }
+
+    private void verificarUsuarioLogado() {
+        autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
+
+        if (autenticacao.getCurrentUser() != null) {
+            abrirTelaPrincipal();
+        }
+
+    }
+
+    private void validarLogin() {
+        autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
+          autenticacao.signInWithEmailAndPassword(usuario.getEmail(), usuario.getSenha())
+            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                        abrirTelaPrincipal();
+                    } else {
+
+                    }
+                }
+            });
+    }
+
+    private void abrirTelaPrincipal() {
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 
     public void abrirCadastroUsuario(View view) {
--- WhatsAppAndsu/app/src/main/java/com/andersonsouza/whatsappandsu/activity/MainActivity.java
+++ WhatsAppAndsu/app/src/main/java/com/andersonsouza/whatsappandsu/activity/MainActivity.java
@@ -1,19 +1,38 @@
 package com.andersonsouza.whatsappandsu.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.andersonsouza.whatsappandsu.R;
 import com.andersonsouza.whatsappandsu.config.ConfiguracaoFirebase;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button botaoSair;
+    private FirebaseAuth autenticacao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        botaoSair = findViewById(R.id.buttonSair);
+
+        botaoSair.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
+                autenticacao.signOut();
+
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 
 }
